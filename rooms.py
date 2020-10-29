73bcff0a556faa10f5b4d00a1d1c5784b358479d
John S <tinco7@aol.com>
2020-10-28 23:18:03 -0700

    rooms table

--- rooms.py
+++ rooms.py
@@ -1,8 +1,24 @@
 import tkinter as tk
-
+from tkinter.ttk import *
 class Window1:
     def __init__(self, master):
-        pass
+        window = Frame(master)
+        label = Label(window, text="Rooms").grid(row=0,column=0)
+        # take the data 
+        lst = [(1,'Raj','Mumbai',19), 
+            (2,'Aaryan','Pune',18), 
+            (3,'Vaishnavi','Mumbai',20), 
+            (4,'Rachna','Mumbai',21), 
+            (5,'Shubham','Delhi',21)] 
+        # find total number of rows and 
+        # columns in list 
+        rows = len(lst) 
+        cols = len(lst[0]) 
+        gridLabels = []
+        for r in range(rows):
+            for c in range(cols):
+                gridLabels.append(Label(window, text=lst[r][c],borderwidth=1 ).grid(row=r+1,column=c+1))
+        window.pack()
         # Create labels, entries,buttons
     def button_click(self):
         pass
@@ -16,7 +32,7 @@ class Window2:
         #run this when button click to close window
         self.master.destroy()
 
-def main(): #run mianloop 
+def main(): #run mainnloop  
     root = tk.Tk()
     app = Window1(root)
     root.mainloop()
