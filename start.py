bc3b1312dda0a629717b1e33721bba105ecea1b8
John S <tinco7@aol.com>
2020-10-31 21:30:49 -0700

    added the room type back and fixed columns on reservation window

--- start.py
+++ start.py
@@ -1,5 +1,8 @@
 import tkinter as tk
 from tkinter.ttk import *
+import datetime as dt
+from PIL import Image
+import os
 class Window1:  # This window is for all the HOTEL ROOMS
     def __init__(self, master):
         window = Frame(master)
@@ -9,10 +12,10 @@ class Window1:  # This window is for all the HOTEL ROOMS
         Label(window, text="Status").grid(row=1, column=1)
         # take the data
         lst = [(101, 'Available'),
-            (102, 'Available'),
-            (103, 'Unavailable/Occupied'),
-            (104, 'Unavailable/Dirty'),
-            (105, 'Unavailable/Maintenance')]
+            (102, 'K','Available'),
+            (103, 'DQ','Unavailable/Occupied'),
+            (104, 'DQK','Unavailable/Dirty'),
+            (105, 'S','Unavailable/Maintenance')]
         # find total number of rows and
         # columns in list
         rows = len(lst)
@@ -79,17 +82,20 @@ class Window3:  # This window is for RESERVATION!
     def __init__(self, master):
         window = Frame(master)
         master.title("RESERVATIONS")
-        Label(window, text="Reservations", background="pink",anchor='W').grid(row=0, column=0)
-        Label(window, text="Room #", background="pink").grid(row=1, column=0)
-        Label(window, text="Guest", background="pink").grid(row=1, column=1)
-        Label(window, text="Date-In", background="pink").grid(row=1, column=2)
-        Label(window, text="Date-Out", background="pink").grid(row=1, column=3)
+        Label(window, text="Reservations", background="pink",anchor='w').grid(row=0, column=0)
+        Label(window, text="First", background="pink").grid(row=1, column=0)
+        Label(window, text="Last", background="pink").grid(row=1, column=1)
+        Label(window, text="Date Made", background="pink").grid(row=1, column=2)
+        Label(window, text="Check-in", background="pink").grid(row=1, column=3)
+        Label(window, text="Check-out", background="pink").grid(row=1, column=4)
+        Label(window, text="Room Type", background="pink").grid(row=1, column=5)
+        Label(window, text="Website", background="pink").grid(row=1, column=6)
+        Label(window, text="Rate", background="pink").grid(row=1, column=7)
+        Label(window, text="Total Charge", background="pink").grid(row=1, column=8)
         # take the data
-        lst = [(101, 'Raj', '11-20-2020', '11-25-2020'),
-            (102, 'Aaryan','11-25-2020','11-28-2020'),
-            (103, 'Vaishnavi','11-19-2020','11-23-2020'),
-            (104, 'Rachna','11-23-2020','11-25-2020'),
-            (105, 'Shubham','11-22-2020','11-23-2020')]
+        lst = [('Raj', 'Mumbai', dt.date(2020, 10, 5), dt.date(2020, 10, 10), dt.date(2020, 10, 15), 'S', 'booking.com', '$7', 5*7),
+            ('Aaryan', 'Pune', dt.date(2020, 10, 5), dt.date(2020, 10, 10), dt.date(2020, 10, 15), 'S', 'booking.com', '$7', 5*7),
+            ('Shubham', 'Delhi', dt.date(2020, 10, 5), dt.date(2020, 10, 10), dt.date(2020, 10, 15), 'S', 'booking.com', '$7', 5*7),]
         # find total number of rows and
         # columns in list
         rows = len(lst)
@@ -110,7 +116,7 @@ class Window4:  # This window is for HOUSEKEEPING!
     def __init__(self, master):
         window = Frame(master)
         master.title("HOUSEKEEPING")
-        label = Label(window, text="Rooms", background="pink",anchor='W').grid(row=0, column=0)
+        label = Label(window, text="Rooms", background="pink",anchor='w').grid(row=0, column=0)
         # take the data
         lst = [('Server', 'Rm#', 'Type', 'Status', 'Bathroom', 'Towels', 'Sheets', 'Vacuum', 'Dusting', 'Electronics'),
             ('', '', '', '', '', '', '', '', '', ''),
@@ -137,12 +143,8 @@ class Window5:  # This window is for GUEST PROFILE!
     def __init__(self, master):
         window = Frame(master)
         master.title("GUEST PROFILE")
-        label = Label(window, text="Guest", background="pink",anchor='W').grid(row=0, column=0)
-        # take the data
+        label = Label(window, text="Guest", background="pink",anchor='w').grid(row=0, column=0)
         lst = [('First Name', 'Last Name', 'Phone', 'Address', 'E-mail',  'ID Info (State, ID#)', 'Vehicle License Plate'),
-            ('', '', '', '', '', '', ''),
-            ('', '', '', '', '', '', ''),
-            ('', '', '', '', '', '', ''),
             ('', '', '', '', '', '', '')]
         # find total number of rows and
         # columns in list
@@ -151,7 +153,7 @@ class Window5:  # This window is for GUEST PROFILE!
         gridLabels = []
         for r in range(rows):
             for c in range(cols):
-                gridLabels.append(Label(window, text=lst[r][c], borderwidth=1).grid(row=r+1, column=c))
+                gridLabels.append(Label(window, text=lst[r][c], borderwidth=1).grid(row=r+2, column=c))
         window.pack()
         # Create labels, entries,buttons
 
@@ -164,7 +166,7 @@ class Window6:  # This window is for GUEST'S CURRENT STAY INFO!
     def __init__(self, master):
         window = Frame(master)
         master.title("GUEST'S CURRENT STAY INFO")
-        label = Label(window, text="Guests", background="pink",anchor='W').grid(row=0, column=0)
+        label = Label(window, text="Guests", background="pink",anchor='w').grid(row=0, column=0)
         # take the data
         lst = [('Guest Name', 'Check In Date and Time', 'Expected Check Out Date and Time',  'Room Type', 'Room Number',
                 'Room Rate ($/Day)', 'Total Charge', 'Payments Made', 'Balance'),
@@ -192,7 +194,7 @@ class Window7:  # This window is for GUEST SEARCH!
     def __init__(self, master):
         window = Frame(master)
         master.title("GUEST SEARCH")
-        label = Label(window, text="Search", background="pink",anchor='W').grid(row=0, column=0)
+        label = Label(window, text="Search", background="pink",anchor='w').grid(row=0, column=0)
         # take the data
         lst = [('Guest First Name', 'Guest Last Name', 'Room Number', 'Phone Number', 'Street Address', 'Check In Date',
                 'Checkout Date'),
@@ -217,7 +219,7 @@ class Window8:  # This window is for DAILY REPORT!
     def __init__(self, master):
         window = Frame(master)
         master.title("DAILY REPORT")
-        label = Label(window, text="Rooms", background="pink",anchor='W').grid(row=0, column=0)
+        label = Label(window, text="Rooms", background="pink",anchor='w').grid(row=0, column=0)
         # take the data
         lst = [('Room Number', 'Guest Name', 'Date In', 'Date Out (if out)', 'Amount Paid'),
             (11, 'Aaryan', '11/12/13', '', '233.34'),
